cdc51eb32c37c43d861860fe87d76ff08d9ef0f6
roylic <lykismekk@outlook.com>
2023-04-04 22:51:48 +0800

    simple filling the http handler -> can be passed into the net/http pkg

--- 017_understanding-net-http-package/01_Handler/main.go
+++ 017_understanding-net-http-package/01_Handler/main.go
@@ -7,10 +7,12 @@ import (
 
 type hotdog int
 
+// any function implement ServeHTTP(ResponseWriter, *Request) would a kind of Http-Handler
 func (m hotdog) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("Any code you want in this func")
 }
 
 func main() {
-
+	var d hotdog
+	http.ListenAndServe(":8080", d)
 }
